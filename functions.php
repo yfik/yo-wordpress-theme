c215e75c4cc5d1a58cf60cba779210c093c29556
yfik <40112762+yfik@users.noreply.github.com>
2020-01-27 18:21:56 +0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -113,6 +113,42 @@ function yo_theme_widgets_init() {
 		'before_title'  => '<h2 class="widget-title">',
 		'after_title'   => '</h2>',
 	) );
+	register_sidebar( array(
+		'name' => 'Footer Sidebar 1',
+		'id' => 'footer-sidebar-1',
+		'description' => 'Appears in the footer area',
+		'before_widget' => '<aside id="%1$s" class="footer-widget %2$s">',
+		'after_widget' => '</aside>',
+		'before_title' => '<h3 class="footer-widget-title">',
+		'after_title' => '</h3>',
+		) );
+	  register_sidebar( array(
+		'name' => 'Footer Sidebar 2',
+		'id' => 'footer-sidebar-2',
+		'description' => 'Appears in the footer area',
+		'before_widget' => '<aside id="%1$s" class="footer-widget %2$s">',
+		'after_widget' => '</aside>',
+		'before_title' => '<h3 class="footer-widget-title">',
+		'after_title' => '</h3>',
+		) );
+	  register_sidebar( array(
+		'name' => 'Footer Sidebar 3',
+		'id' => 'footer-sidebar-3',
+		'description' => 'Appears in the footer area',
+		'before_widget' => '<aside id="%1$s" class="footer-widget %2$s">',
+		'after_widget' => '</aside>',
+		'before_title' => '<h3 class="footer-widget-title">',
+		'after_title' => '</h3>',
+		) );
+	  register_sidebar( array(
+		'name' => 'Footer Sidebar 4',
+		'id' => 'footer-sidebar-4',
+		'description' => 'Appears in the footer area',
+		'before_widget' => '<aside id="%1$s" class="footer-widget %2$s">',
+		'after_widget' => '</aside>',
+		'before_title' => '<h3 class="footer-widget-title">',
+		'after_title' => '</h3>',
+		) );
 }
 add_action( 'widgets_init', 'yo_theme_widgets_init' );
 // Adding Bootstrap
@@ -237,6 +273,7 @@ class yo_theme_navbar extends Walker_Nav_Menu
         }
     }
 }
+
 
 /**
  * Implement the Custom Header feature.
