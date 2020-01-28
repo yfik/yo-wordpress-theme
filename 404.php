4ec30816cb2495d4a25548b84e2d3d830ed297b6
yfik <40112762+yfik@users.noreply.github.com>
2020-01-28 14:25:02 +0300

    Update 404.php

--- 404.php
+++ 404.php
@@ -9,19 +9,19 @@
 
 get_header();
 ?>
-
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
-
-			<section class="error-404 not-found">
+<div class="container">
+  <div class="row">
+    <div id="yo_content" class="col-sm-7">
 				<header class="page-header">
 					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'yo-theme' ); ?></h1>
 				</header><!-- .page-header -->
 
 				<div class="page-content">
 					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'yo-theme' ); ?></p>
-
-					<?php
+				</div>
+</div>
+				<div id="yo_sidebar" class="col-sm-4">
+				<?php
 					get_search_form();
 
 					the_widget( 'WP_Widget_Recent_Posts' );
@@ -51,10 +51,9 @@ get_header();
 					?>
 
 				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+				
+	</div>		
+</div><!-- #primary -->
 
 <?php
 get_footer();
