572416664c817415daa08caaf5a61ba537de6fbb
yfik <40112762+yfik@users.noreply.github.com>
2020-01-28 15:33:25 +0300

    Update footer.php

--- footer.php
+++ footer.php
@@ -6,36 +6,46 @@
  *
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
  *
- * @package yo_theme
+ * @package yo_fik
  */
 
 ?>
 
-	</div><!-- #content -->
-	<nav id="site-navigation"  class="navbar navbar-expand-md navbar-dark top bg-dark">
-    <a class="navbar-brand" href="#">
-        <?php bloginfo('name'); ?>
-    </a>
-    <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'yo_theme' ); ?></button>
-	<?php
-            wp_nav_menu([
-            'menu'            => 'footer menu',
-            'theme_location'  => 'menu-1',
-            'container'       => 'div',
-            'container_id'    => 'navbarCollapse',
-            'container_class' => 'collapse navbar-collapse',
-            'menu_id'         => false,
-            'menu_class'      => 'navbar-nav mr-auto',
-            'depth'           => 0,
-            'fallback_cb'     => 'functions::fallback',
-            'walker'          => new yo_theme_navbar()
-            ]);
-        ?>
-		</nav>
-		
-		</div>
+
 
 	<footer id="colophon" class="site-footer">
+	<div class="container-fluid text-center text-md-left">
+      <div id="footer-sidebar" class="row secondary">
+        <div class="col-md-3" id="footer-sidebar1">
+        <?php
+        if(is_active_sidebar('footer-sidebar-1')){
+        dynamic_sidebar('footer-sidebar-1');
+        }
+        ?>
+        </div>
+        <div class="col-md-3" id="footer-sidebar2">
+        <?php
+        if(is_active_sidebar('footer-sidebar-2')){
+        dynamic_sidebar('footer-sidebar-2');
+        }
+        ?>
+        </div>
+        <div class="col-md-3" id="footer-sidebar3">
+        <?php
+        if(is_active_sidebar('footer-sidebar-3')){
+        dynamic_sidebar('footer-sidebar-3');
+        }
+        ?>
+        </div>
+        <div class="col-md-3" id="footer-sidebar4">
+        <?php
+        if(is_active_sidebar('footer-sidebar-4')){
+        dynamic_sidebar('footer-sidebar-4');
+        }
+        ?>
+        </div>
+      </div>
+
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'yo-theme' ) ); ?>">
 				<?php
