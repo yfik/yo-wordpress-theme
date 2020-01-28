1697da77593f354021f0b13b0f60cc724506017e
yfik <40112762+yfik@users.noreply.github.com>
2020-01-28 15:33:07 +0300

    Update 404.php

--- 404.php
+++ 404.php
@@ -4,24 +4,24 @@
  *
  * @link https://codex.wordpress.org/Creating_an_Error_404_Page
  *
- * @package yo_theme
+ * @package yo_fik
  */
 
 get_header();
 ?>
-
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
-
-			<section class="error-404 not-found">
+<div class="container">
+  <div class="row">
+    <div id="yo_content" class="col-sm-7">
 				<header class="page-header">
 					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'yo-theme' ); ?></h1>
 				</header><!-- .page-header -->
 
 				<div class="page-content">
 					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'yo-theme' ); ?></p>
-
-					<?php
+				</div>
+</div>
+				<div id="yo_sidebar" class="col-sm-4">
+				<?php
 					get_search_form();
 
 					the_widget( 'WP_Widget_Recent_Posts' );
@@ -44,17 +44,16 @@ get_header();
 
 					<?php
 					/* translators: %1$s: smiley */
-					$yo_theme_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'yo-theme' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$yo_theme_archive_content" );
+					$yo_fik_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'yo-theme' ), convert_smilies( ':)' ) ) . '</p>';
+					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$yo_fik_archive_content" );
 
 					the_widget( 'WP_Widget_Tag_Cloud' );
 					?>
 
 				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+				
+	</div>		
+</div><!-- #primary -->
 
 <?php
 get_footer();
